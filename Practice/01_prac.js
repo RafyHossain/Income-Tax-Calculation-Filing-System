68b37bc6cbde42b8440e97349118231bc7934ac6
RafyHossain <rafyhossain56@gmail.com>
2026-07-10 23:36:09 +0600

    Setup Express

---
+++ Practice/01_prac.js
@@ -0,0 +1,21 @@
+const taxpayers = [
+    {
+        id: 101,
+        name: "Rafy",
+        income: 500000,
+        status: "Approved"
+    },
+    {
+        id: 102,
+        name: "Hasan",
+        income: 900000,
+        status: "Pending"
+    },
+    {
+        id: 103,
+        name: "Karim",
+        income: 1200000,
+        status: "Approved"
+    }
+];
+
